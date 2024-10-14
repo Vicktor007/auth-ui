56aed54793e6d4e83216f8d2e70c8a2c84f7c9c2
Vicktor007 <vicktord007@gmail.com>
2024-10-14 21:43:48 +0100

    oauth ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import Register from './components/Register';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          
+          {/* Homepage */}
+          <Route path="/home" element={<Home />} />
+          
+          {/* Login page */}
+          <Route path="/login" element={<Login/>} />
+          
+          {/* Registration page */}
+          <Route path="/register" element={<Register  />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
